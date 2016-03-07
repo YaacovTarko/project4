6772fd20917f6b701f0cb9f9d312ea721c5e599f
Yaacov Tarko <yaacov.tarko@gmail.com>
2016-03-07 13:44:52 -0800

    Map finished, but not fully tested

---
+++ InteractionTuple.h
@@ -0,0 +1,13 @@
+//
+//  InteractionTuple.h
+//  project 4
+//
+//  Created by Yaacov Tarko on 3/7/16.
+//  Copyright (c) 2016 Yaacov Tarko. All rights reserved.
+//
+
+#ifndef project_4_InteractionTuple_h
+#define project_4_InteractionTuple_h
+
+
+#endif
